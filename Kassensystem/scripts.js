8f172d1d221a5d099f700dd6b8e2b66aafaddf29
Niklas <Niklas@Ardey.com>
2018-07-16 09:04:20 +0200

    Auf aktuellen stand gebracht

---
+++ Kassensystem/scripts.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function loginPopup(){
+    var username = prompt("please enter your username", "Username"); 
+    var password = prompt("please enter your password", "Password"); 
+    var attempts = 0;
+//    while(attempts < 3) {
+//        
+//    }
+}
+
