290ddc345be84d64dccb0271f35586112a87277b
Niklas <Niklas@Ardey.com>
2018-07-17 20:39:09 +0200

    Weiter php gelernt

--- HTML/projects/learningPhp/index.php
+++ HTML/projects/learningPhp/index.php
@@ -17,7 +17,52 @@
         $bool = true; //boolean 
         // "." verbindet Strings miteinander
         echo "Test".$test;
-        echo "<h1>$test</h1>"
+        echo "<h1>$test</h1>"; 
+        // Arrays
+        $geschlechter = array("m", "f");
+        echo "Ich bin " . $geschlechter[0];
+
+        //if else Anweisung
+        if($bool){
+            echo "Test";
+        } elseif{
+
+        } else{
+            echo "Anderer Test";
+        }
+
+        //switch-case-Statement
+        switch ($x) {
+            case '1':
+                # code...
+                break;
+            case '2':
+                # code...
+                break;
+            default:
+                # code...
+                break;
+        }
+
+        //while Schleifen
+
+        while ($x <= 10) {
+            echo "Test".$x += 1
+        }
+
+        //Do While 
+
+        do {
+            echo "test".$x += 1 
+        } while ($x <= 10);
+
+        //for Schleife
+        for ($x=0; $x < 2; $x++) { 
+            # code...
+        }
+        foreach ($variable as $key) {
+            # code...
+        }
     ?>
 </body>
 </html>
